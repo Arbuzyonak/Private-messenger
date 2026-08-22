31c03e23123a434701059b72e65ca6460b1c0406
Arbuz <nassif28ek@gmail.com>
2026-08-21 23:10:01 -0400

    Added a better read me and fixed some networking bugs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Private-messenger
+
+**This was made in about 25 hours & its my first time doing nettworking**
+
+# What is this?
+
+This is a messenger that you can use on your own local network.
+
+# How to make it work?
+
+Launch the server and have at least two instances of the client running then on each client add the other's username.
+
+# Showcase
--- server_side/server.py
+++ server_side/server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json
 
 
 HEADER = 64
@@ -10,25 +11,165 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(ADDR)
+
+clients = {}
+clients_lock = threading.Lock()
+
+
+def recv_exact(conn, size):
+    data = b""
+
+    while len(data) < size:
+        chunk = conn.recv(size - len(data))
+        if not chunk:
+            return None
+        data += chunk
+
+    return data
+
+
+def send_packet(conn, packet):
+    message = json.dumps(packet).encode(FORMAT)
+    send_length = str(len(message)).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+
+    conn.sendall(send_length)
+    conn.sendall(message)
+
+
+def receive_packet(conn):
+    msg_length = recv_exact(conn, HEADER)
+
+    if not msg_length:
+        return None
+
+    msg_length = int(msg_length.decode(FORMAT).strip())
+    message = recv_exact(conn, msg_length)
+
+    if not message:
+        return None
+
+    return json.loads(message.decode(FORMAT))
+
+
+def find_client_by_username(username):
+    with clients_lock:
+        for user_id, client in clients.items():
+            if client["username"] == username:
+                return user_id, client
+
+    return None, None
+
 
 def handle_client(conn, addr):
     print(f"New connection: {addr} connected.")
 
+    current_user_id = None
+    current_username = None
+    send_lock = threading.Lock()
+
     connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
+
+    try:
+        while connected:
+            packet = receive_packet(conn)
+
+            if packet is None:
+                break
+
+            if packet["type"] == "register":
+                user_id = packet["user_id"]
+                username = packet["username"]
+
+                current_user_id = user_id
+                current_username = username
+
+                with clients_lock:
+                    clients[user_id] = {
+                        "username": username,
+                        "socket": conn,
+                        "send_lock": send_lock
+                    }
+
+                print(f"{username} connected")
+                print(clients)
+
+            elif packet["type"] == "add_contact":
+                username = packet["username"]
+                contact_user_id, contact = find_client_by_username(username)
+
+                if contact is None:
+                    with send_lock:
+                        send_packet(conn, {
+                            "type": "contact_error",
+                            "error": "User is not online"
+                        })
+                    continue
+
+                if contact_user_id == current_user_id:
+                    with send_lock:
+                        send_packet(conn, {
+                            "type": "contact_error",
+                            "error": "You cannot add yourself"
+                        })
+                    continue
+
+                with send_lock:
+                    send_packet(conn, {
+                        "type": "contact_added",
+                        "username": contact["username"],
+                        "user_id": contact_user_id
+                    })
+
+                with contact["send_lock"]:
+                    send_packet(contact["socket"], {
+                        "type": "contact_added",
+                        "username": current_username,
+                        "user_id": current_user_id
+                    })
+
+            elif packet["type"] == "message":
+                recipient_id = packet["recipient_id"]
+                text = packet["text"]
+
+                with clients_lock:
+                    recipient = clients.get(recipient_id)
+
+                if recipient:
+                    print(f"Sending {text} to {recipient_id}")
+
+                    with recipient["send_lock"]:
+                        send_packet(recipient["socket"], {
+                            "type": "message",
+                            "sender_username": current_username,
+                            "sender_user_id": current_user_id,
+                            "text": text
+                        })
+                else:
+                    with send_lock:
+                        send_packet(conn, {
+                            "type": "message_error",
+                            "error": "Recipient is offline"
+                        })
+
+            elif packet["type"] == "disconnect":
                 connected = False
 
-            print(f"{addr}: {msg}")
-            conn.send(f"Msg received {msg}".encode(FORMAT))
+            print(f"{addr}: {packet}")
 
-    conn.close()
+    except (OSError, ValueError, json.JSONDecodeError, KeyError):
+        pass
+
+    finally:
+        if current_user_id is not None:
+            with clients_lock:
+                current_client = clients.get(current_user_id)
+                if current_client and current_client["socket"] is conn:
+                    del clients[current_user_id]
+
+        conn.close()
 
 
 
@@ -43,4 +184,5 @@ def start():
 
 
 print("Server is starting...")
-start()
+if __name__ == "__main__":
+    start()
